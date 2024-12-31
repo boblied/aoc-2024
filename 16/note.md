2b7d54eef019d0783b8b38bffbbccd4e7c514a40
Bob Lied <boblied+github@gmail.com>
2024-12-31 08:36:21 -0600

    AOC 2024 Day 16 done

--- 16/note.md
+++ 16/note.md
@@ -13,14 +13,40 @@ a compare on scalar values (the array reference), not on the list
 contents.
 
 Extra tests found on Reddit.
-test1 21148
-test2 5078
+      Part 1    Part 2
+test1 21148     149
+test2 5078      413
 
 ## Part 2
 
 That List::PriorityQueue is really getting in my way. To save the path,
-I need to pass along a referce to an array to update, but if I include it
+I want to pass along a referce to an array to update, but if I include it
 in the key, it has to be part of the string.
 
-test1 149
-test2 413
+Turns out I don't need to record the path, saving the distance table is enough.
+
+Use Dijkstra algorithm to get distance from start to end for every trail.
+Now follow the trail backward, but use the distance info to follow alternatives.
+
+  #    #    #    #    #    #    #    #    #    #    #    #    #    #    #    
+  #                                       #                       36    #    
+  #         #         #    #    #         #         #    #    #   35    #    
+  #                             #         #                   #   34    #    
+  #         #    #    #         #    #    #    #    #         #   33    #    
+  #         #         #                                       #   32    #    
+  #         #         #    #    #    #    #         #    #    #   31    #    
+  #              8    9   10   11   12   13   14   15   16    #   30    #    
+  #    #    #    7    #    9    #    #    #    #    #   17    #   29    #    
+  #    4    5    6    #    8                        #   18    #   28    #    
+  #   -3-   #   -5-   #    7    #    #    #         #   19    #   27    #    
+  #   [2]  -3-  [4]  -5-   6    #                   #   20    #   26    #    
+  #    1    #    #    #         #         #         #   21    #   25    #    
+  #    0              #                             #   22   23   24    #    
+  #    #    #    #    #    #    #    #    #    #    #    #    #    #    #    
+
+Switch to a different heap implementation (Array::Heap::PriorityQueue::Numeric)
+that allows array references as values.
+
+Last step, to check coverage while moving from End to Start, is a breadth-first
+search, using the distances to decide what is valid in the next fringe.
+The distance table has the directions, but obviously in the opposite direction.
--- 16/part2.pl
+++ 16/part2.pl
@@ -12,6 +12,8 @@
 #=============================================================================
 
 use v5.40;
+use List::Util qw/min/;
+
 use FindBin qw($Bin); use lib "$FindBin::Bin/../../lib"; use AOC;
 use AOC::Grid;
 
@@ -24,11 +26,19 @@ my $Maze = AOC::Grid::loadGrid();
 my @Start = ($Maze->findChar('S'))[0]->@*;
 my @End   = ($Maze->findChar('E'))[0]->@*;
 
-my $answer = coverBest($TargetScore, $Maze, '>', @Start, @End);
+my $distance = dijkstra($Maze, '>', @Start, @End);
+
+# Now walk backwards from the end. Keep decrementing (-1 for straight,
+# -1000 for a turn) and include the tile as long as the remaining length
+# is positive.
+
+my $answer = cover($Maze, $distance, @End, @Start);
 say $answer;
 
 $logger->info("FINISH");
 
+# Make CSV of a list so it can be a key in a priority queue.
+sub str(@stuff) { local $" = ","; qq(@stuff) }
 
 sub neighbor($r, $c, $dir)
 {
@@ -43,44 +53,36 @@ state %t = ( '^' => [ qw(< >) ], '>' => [ qw(^ v) ],
     return $t{$dir}->@*;
 }
 
-sub coverBest($targetScore, $maze, $direction, $startR, $startC, @end)
+sub dijkstra($maze, $direction, $startR, $startC, @end)
 {
-    my %distance;
     my $bestScore = $maze->height * $maze->width * 1001;
 
-    my %pathInBest = ( "$startR $startC" => true, "@end" => true );
+    my %distance;
+    $distance{ str($startR,$startC)}{$direction } = 0;
 
-    my $ptDir = "$startR $startC $direction";
-    $distance{$ptDir} = 0;
+    my %seen;
+    
+    use Array::Heap::PriorityQueue::Numeric;
+    my $pq = Array::Heap::PriorityQueue::Numeric->new();
+    $pq->add( [ $startR, $startC, $direction, [] ],  0);
 
-    my %visited;
-
-    my %prio;
-    $prio{$ptDir} = { r=>$startR, c=>$startC, dir=>$direction,
-                      prio=>0, path=>[ "$startR $startC" ] };
-
-    while ( my $x = lowestPriority(\%prio) )
+    while ( my $x = $pq->get() )
     {
-        my $r = $x->{r};
-        my $c = $x->{c};
-        my $dir = $x->{dir};
-        my $pathSoFar = $x->{path};
-
-        my $ptDir = "$r $c $dir";
-        my $score = $distance{$ptDir};
+        my ($r, $c, $dir, $path) = $x->@*;;
+        my $score = $distance{ str($r,$c)}{$dir };
 
         if ( "$r $c" eq "@end" )
         {
-            $logger->debug("END score=$score");
-            if ( $score == $targetScore )
+            if ( $score < $bestScore )
             {
-                $pathInBest{$_} = true for $pathSoFar->@*;
+                $bestScore = $score;
+                $logger->debug("Saved path for score=$score, length=", $path->$#*+2);
             }
             next;
         }
 
-        next if $visited{$ptDir};
-        $visited{$ptDir} = true;
+        next if $seen{ str($r,$c,$dir) };
+        $seen{ str($r,$c,$dir) } = true;
 
         for ( $dir, validTurn($dir) )
         {
@@ -88,28 +90,55 @@ sub coverBest($targetScore, $maze, $direction, $startR, $startC, @end)
             my $v = $maze->get(@n);
             next if $v eq '#';
 
-            $ptDir = "@n $_";
             my $cost = $score + 1 + ( $_ eq $dir  ? 0 : 1000 );
-            if ( ! exists $distance{$ptDir} || $cost < $distance{$ptDir} )
+            if ( ! exists $distance{ str($n[0],$n[1])}{$_ }
+                || $cost < $distance{ str($n[0],$n[1])}{$_ } )
             {
-                $distance{$ptDir} = $cost
+                $distance{ str($n[0],$n[1])}{$_ } = $cost;
             }
 
-            next if $cost > $targetScore;
+            next if $cost > $bestScore;
 
-            $prio{$ptDir} = { r=>$n[0], c=>$n[1], dir=>$_,
-                              prio=>$cost,
-                              path=>[ $pathSoFar->@*, "@n" ] };
+            $pq->add( [ @n, $_, [ @$path, [$r, $c] ] ], $cost );
         }
     }
-    return scalar(keys %pathInBest);
+
+    return \%distance;
 }
 
-sub lowestPriority($pq)
+sub cover($maze, $distance, $startR, $startC, $endR, $endC)
 {
-    return undef if scalar(keys %$pq) == 0;
-    my @byPriority = sort { $pq->{$a}{prio} <=> $pq->{$b}{prio} } keys %$pq;
-    my $lowest = $pq->{$byPriority[0]};
-    delete $pq->{$byPriority[0]};
-    return $lowest;
+    my %covered;
+    my @queue;
+    my %seen;
+    my $budget = min  values %{$distance->{str($startR, $startC)}};
+
+    push @queue, [ $startR, $startC, $budget ];
+    while ( my $x = shift @queue )
+    {
+        my ($r, $c, $remain) = @$x;
+
+        $covered{str($r,$c)} = true;
+        last if ( str($r,$c) eq str($endR, $endC) );
+
+        next if $seen{$r}{$c};
+        $seen{$r}{$c} = true;
+        
+        my $intoHere = $distance->{str($r,$c)};
+        my @incomingDir = grep { $intoHere->{$_} <= $remain } keys %$intoHere;
+
+        state %reverse = ( '^' => 'v', 'v' => '^', '>' => '<', '<' => '>' );
+
+        for ( @incomingDir )
+        {
+            my @n = neighbor($r, $c, $reverse{$_});
+            my $d = $distance->{str($r,$c)}{$_};
+            if ( $remain - $d >= 0 )
+            {
+                push @queue, [ @n, $d ];
+            }
+        }
+    }
+
+    return scalar(keys %covered);
 }
